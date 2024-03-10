b4ef4eb847de24c958069a247e586120ef6aee04
Pataxsa <cvjeticaxel@gmail.com>
2024-03-10 23:24:43 +0100

    Ajout d'un test de l'api dans le main

--- main.py
+++ main.py
@@ -4,7 +4,15 @@ from requests.exceptions import HTTPError
 from utils.api import Api
 
 #Test de l'api (a continuer)
+#Liste toutes les communes disponibles
 try:
-    print(Api().getLines(size=1))
+    results = None
+    b = 1
+    while results == None or len(results) != 0:
+        results = Api().getLines(page=b, qs="type_de_structure:Collectivité territoriale AND type_de_collectivite:Communes", select="raison_sociale")["results"]
+        b += 1
+
+        for i in range(len(results)):
+            print(results[i])
 except HTTPError as e:
     print("Error: " + str(e.response))
