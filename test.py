f5dd096366da3894205b0e559639d33c582633a7
Pataxsa <cvjeticaxel@gmail.com>
2024-03-17 19:42:10 +0100

    test python

--- test.py
+++ test.py
@@ -2,7 +2,8 @@ from utils.libs import checkLibs
 checkLibs()
 from utils.gui import Gui
 
-gui = Gui("Recherche GES par Ville", False)
-
 #Effectue un test de l'initialisation
-gui.testinit()
+def test():
+    gui = Gui("Recherche GES par Ville", False)
+
+    gui.testinit()
