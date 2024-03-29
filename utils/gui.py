f3d686788b77999fbedddcc2b3d4c01b031cce4a
Noam <noamchamouard@outlook.fr>
2024-03-29 16:43:20 +0100

    erreur de syntaxe à tri_France

--- utils/gui.py
+++ utils/gui.py
@@ -106,7 +106,7 @@ class Gui:
     def __show_graphic(self):
         try:
             inputdata = self.list_ville.get()
-            data = self.api.tri_france(self.dataname,inputdata)
+            data = self.api.tri_France(self.dataname,inputdata)
             dates = list(data.keys())
             totalco2 = list(data.values())
 
