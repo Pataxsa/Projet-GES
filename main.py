67607e790671fc7b554d0799dd5ab10f0748604c
Noam <noamchamouard@outlook.fr>
2024-04-05 09:31:51 +0200

    test de vérification pour ne pas réinstaller les modules à chaque lancement

--- main.py
+++ main.py
@@ -7,15 +7,24 @@ from utils.gui import Gui
 
 #Installation des modules nécessaires
 
+modules = ['folium','branca','requests','matplotlib','customtkinter']
+possession = subprocess.run(["pip", "list"], capture_output=True, text=True).stdout.split()
+possession = [possession[i] for i in range(0,len(possession),2)]
 
-try:
-    subprocess.run(["pip","install","poetry"])
-    subprocess.run(["poetry","install"])
 
-    print("Poetry a été installé avec succès!")
 
-except subprocess.CalledProcessError as e:
-    print(f"Une erreur s'est produite lors de l'installation de Poetry : {e}")
+
+for i in modules:
+    if not i in possession:
+        try:
+            subprocess.run(["pip","install","poetry"])
+            subprocess.run(["poetry","install"])
+
+            print("Poetry a été installé avec succès!")
+            break
+
+        except subprocess.CalledProcessError as e:
+            print(f"Une erreur s'est produite lors de l'installation de Poetry : {e}")
 
 
 #Crée une interface
