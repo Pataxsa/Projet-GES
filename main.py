00ab184de147094f04fd26aa2b6a030df8353b25
Noam <noamchamouard@outlook.fr>
2024-04-05 09:07:05 +0200

    mise à jour du fichier d'installation des dépendances ( poetry.lock )

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 """
 Module main pour créer et lancer l'interface
 """
+
 import subprocess
 from utils.gui import Gui
 
 #Installation des modules nécessaires
 
-"""
+
 try:
     subprocess.run(["pip","install","poetry"])
     subprocess.run(["poetry","install"])
@@ -15,7 +16,7 @@ try:
 
 except subprocess.CalledProcessError as e:
     print(f"Une erreur s'est produite lors de l'installation de Poetry : {e}")
-"""
+
 
 #Crée une interface
 gui = Gui("Recherche GES par Lieux")
