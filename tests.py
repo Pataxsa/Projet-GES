0a50574cded620b997903e1087c9f63eeda1b3d2
Noam <noamchamouard@outlook.fr>
2024-04-26 09:14:59 +0200

    tests de l'interface terminés, merge du fichier test vers GUI, début des essais pour intégrer la map dans la fenêtre avec WebView

--- tests.py
+++ tests.py
@@ -1,8 +1,8 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QPushButton, QHBoxLayout, QVBoxLayout, QMessageBox, QComboBox,QSizePolicy
-from PyQt5.QtCore import Qt, QUrl
-from PyQt5.QtWebEngineWidgets import QWebEngineView
-
+from PySide6.QtWidgets import QApplication, QMainWindow, QWidget, QPushButton, QHBoxLayout, QVBoxLayout, QMessageBox, QComboBox,QSizePolicy
+from PySide6.QtCore import Qt, QUrl
+from PySide6.QtGui import QPalette
+from PySide6.QtWebEngineWidgets import QWebEngineView
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
 from os.path import isfile
@@ -29,7 +29,7 @@ class MainWindow(QMainWindow):
         # Créer un layout vertical pour le widget central
         central_layout = QVBoxLayout(central_widget)
         central_layout.setContentsMargins(20, 20, 20, 20)
-        central_layout.setAlignment(Qt.AlignCenter)  # Centrer les éléments verticalement
+        central_layout.setAlignment(Qt.AlignmentFlag.AlignCenter)  # Centrer les éléments verticalement
 
         # Créer une combobox principale
         self.list_ville = QComboBox(self)
@@ -37,11 +37,14 @@ class MainWindow(QMainWindow):
         self.list_ville.setStyleSheet("QComboBox { min-width: 200px; }")  # Définir une largeur minimale
         self.list_ville.currentIndexChanged.connect(self.on_main_combo_box_changed)
 
-        central_layout.addWidget(self.list_ville, alignment=Qt.AlignCenter)  # Centrer la combobox
+        central_layout.addWidget(self.list_ville, alignment=Qt.AlignmentFlag.AlignHCenter)  # Centrer la combobox horizontalement
         self.ignore_main_combo_box_change = False
 
         # Créer un layout horizontal pour les boutons
         button_layout = QHBoxLayout()
+        button_layout.setAlignment(Qt.AlignmentFlag.AlignHCenter)
+        button_layout.setContentsMargins(0, 10, 0, 0)
+        button_layout.setSpacing(20)
         central_layout.addLayout(button_layout)
 
         self.research_button = QPushButton("Générer un graphique", self)
@@ -58,14 +61,20 @@ class MainWindow(QMainWindow):
         self.research_button.clicked.connect(self.__show_graphic)
         self.map_button.clicked.connect(self.__generatemap)
 
+
+        """ A faire, en cours de test
         self.web_view = QWebEngineView(self)
-        central_layout.addWidget(self.web_view, alignment=Qt.AlignCenter)
+        central_layout.addWidget(self.web_view, alignment=Qt.AlignmentFlag.AlignCenter)
+        """
+
 
         # Créer le FigureCanvas pour afficher le graphique
         self.figure = Figure()
-        self.canvas = FigureCanvas(self.figure)
-        central_layout.addWidget(self.canvas, alignment=Qt.AlignCenter)
-        self.canvas.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        self.figure.set_facecolor(self.palette().color(QPalette.ColorGroup.Current, QPalette.ColorRole.Dark).name()) # Mettre le fond du canvas a la même couleur que le fond du menu
+        self.canvas: FigureCanvas = FigureCanvas(self.figure)
+        central_layout.addWidget(self.canvas)
+        self.canvas.setMaximumSize(1000, 800) # Taille max du canvas
+        self.canvas.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding) # Fait en sorte que le canvas puisse s'étendre
     
     def init():
         if isfile("map.html"):
@@ -74,7 +83,7 @@ class MainWindow(QMainWindow):
         app = QApplication(sys.argv)
         window = MainWindow()
         window.show()
-        sys.exit(app.exec_())
+        sys.exit(app.exec())
 
     def on_main_combo_box_changed(self):
         if self.ignore_main_combo_box_change:
@@ -112,7 +121,7 @@ class MainWindow(QMainWindow):
 
     def __generatemap(self):
         self.map.save("map.html")
-        #self.web_view.setUrl(QUrl.fromLocalFile("map.html"))
+
 
     def __show_graphic(self):
         try:
@@ -130,66 +139,19 @@ class MainWindow(QMainWindow):
 
                 # Créer le graphique
                 ax.bar(dates, totalco2, label="CO2")
-                ax.set_title(f"Bilan GES {self.dataname[0:-1]} : {inputdata}")
-                ax.set_xlabel('Dates')
-                ax.set_ylabel('Tonnes de CO2')
+                ax.set_title(f"Bilan GES, {self.dataname[0:-1]} : {inputdata}", color="white")
+                ax.set_xlabel('Dates',  color="white")
+                ax.set_ylabel('Tonnes de CO2',  color="white")
                 ax.legend()
                 ax.tick_params(axis='x', rotation=90)
+                ax.tick_params(labelcolor='white')
 
                 # Mettre à jour le graphique dans le canvas
                 self.figure.subplots_adjust(bottom=0.25)
                 self.canvas.draw()
-    
-
         except HTTPError as e:
             error_message = f"Erreur de requête vers l'API: {str(e.response)}"
             QMessageBox.critical(None, "Erreur", error_message)
-    
-    def resizeEvent(self, event):
-        super().resizeEvent(event)
-
-        window_width = event.size().width()
-        window_height = event.size().height()
-
-        canvas_margin = 50
-        taille_elem = 100  # Hauteur des autres éléments (combobox / boutons)
-
-        canvas_width = window_width - 2 * canvas_margin
-        canvas_height = window_height - 2 * canvas_margin - taille_elem
-
-        # Limiter la taille maximale du canvas
-        max_canvas_width = 1000
-        max_canvas_height = 800
-
-        canvas_width = min(canvas_width, max_canvas_width)
-        canvas_height = min(canvas_height, max_canvas_height)
-
-        # Positionner le canvas au centre de la fenêtre
-        canvas_x = (window_width - canvas_width) // 2
-        canvas_y = (window_height - canvas_height - taille_elem) // 2
-
-        self.canvas.setMinimumSize(int(window_width * 0.9), int(window_height * 0.9))
-        self.canvas.move(canvas_x, canvas_y)
-
-        # Positionner le QComboBox au centre horizontalement
-        combo_box_width = 150
-        combo_box_height = 30
-        combo_box_x = (window_width - combo_box_width) // 2
-        combo_box_y = canvas_y - combo_box_height - 20
-
-        self.list_ville.setGeometry(combo_box_x, combo_box_y+10, combo_box_width, combo_box_height)
-
-        # Positionner les boutons au centre horizontalement
-        button_width = 150
-        button_height = 30
-        button_margin = 20
-
-        button_x = (window_width - 2 * button_width - button_margin ) // 2
-        button_y = canvas_y + canvas_height + button_margin
-
-        self.research_button.setGeometry(button_x, button_y, button_width, button_height)
-        self.map_button.setGeometry(button_x + button_width, button_y, button_width, button_height)
-
 
 
 MainWindow.init()
--- utils/gui.py
+++ utils/gui.py
@@ -1,203 +1,157 @@
-"""
-Module gui pour créer une interface
-"""
-
-import tkinter as tk
-from tkinter import ttk
-from tkinter import messagebox
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg as Figure
-import matplotlib.pyplot as plt
+import sys
+from PySide6.QtWidgets import QApplication, QMainWindow, QWidget, QPushButton, QHBoxLayout, QVBoxLayout, QMessageBox, QComboBox,QSizePolicy
+from PySide6.QtCore import Qt, QUrl
+from PySide6.QtGui import QPalette
+from PySide6.QtWebEngineWidgets import QWebEngineView
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.figure import Figure
 from os.path import isfile
 from os import remove
 from requests.exceptions import HTTPError
 from utils.api import Api
 from utils.map import MAP
-from customtkinter import CTkButton, CTkLabel
-from PIL import Image, ImageTk
 
 
-class Gui:
-    """
-    Classe Gui qui génère une interface
-    Doc customtkinter: https://customtkinter.tomschimansky.com/documentation/
-    """
+class MainWindow(QMainWindow):
+    def __init__(self, title: str = "Emissions de GES par types de localités") -> None:
+        super().__init__()
+        self.setWindowTitle(title)
+        self.setMinimumSize(800,600) #pas de pb avec les redimensionnements
 
-    # Initialisation de la classe et des fenetres avec ses composants
-    def __init__(self, title: str = "Title", resizable: bool = True, tests: bool = False) -> None:
-        # Initialisation de l'API
-        try:
-            self.api = Api()
-        except HTTPError as e:
-            if tests: raise e
-            messagebox.showerror("Erreur", "Erreur de requete vers l'API: " + str(e.response))
-
-        # Initialisation de la carte
+        self.api = Api()
         self.map = MAP(self.api)
-
-        # Initialisation des ressources de l'interface
-        self.img = Image.open(f"{self.api.basepath}\\interface\\img\\GES.jpg")
-
-        # Initialisation des composants de l'interface
-        self.window = tk.Tk()
-        self.canvas = tk.Canvas(self.window)
-        self.background_photo = None
-        self.list_ville = ttk.Combobox(self.window, width=len(self.api.communes[0]) + 5, state="readonly")
-        self.ville_label = CTkLabel(self.window, text="Commune :", text_color="black", height=20,padx=3,pady=3)
-        self.research_button = CTkButton(self.window, text="Rechercher", command=self.__show_graphic)
-        self.map_button = CTkButton(self.window, text="Générer une carte", command=self.__generatemap)
-        self.graphic_widget = None
-
-        # Paramètres de l'interface
-        self.title = title
-        self.resizable = resizable
-        self.minsize = (400, 200)
-        self.tests = tests
-
-        # Parametres de l'API
         self.dataname = "Communes"
 
-    def init(self) -> None:
-        """
-        Fonction init pour lancer une interface
+        # Créer un widget central pour la fenêtre
+        central_widget = QWidget(self)
+        self.setCentralWidget(central_widget)
+
+        # Créer un layout vertical pour le widget central
+        central_layout = QVBoxLayout(central_widget)
+        central_layout.setContentsMargins(20, 20, 20, 20)
+        central_layout.setAlignment(Qt.AlignmentFlag.AlignCenter)  # Centrer les éléments verticalement
+
+        # Créer une combobox principale
+        self.list_ville = QComboBox(self)
+        self.list_ville.addItems(["Choisissez le type de localité","Commune", "Région", "Département"])
+        self.list_ville.setStyleSheet("QComboBox { min-width: 200px; }")  # Définir une largeur minimale
+        self.list_ville.currentIndexChanged.connect(self.on_main_combo_box_changed)
+
+        central_layout.addWidget(self.list_ville, alignment=Qt.AlignmentFlag.AlignHCenter)  # Centrer la combobox horizontalement
+        self.ignore_main_combo_box_change = False
+
+        # Créer un layout horizontal pour les boutons
+        button_layout = QHBoxLayout()
+        button_layout.setAlignment(Qt.AlignmentFlag.AlignHCenter)
+        button_layout.setContentsMargins(0, 10, 0, 0)
+        button_layout.setSpacing(20)
+        central_layout.addLayout(button_layout)
+
+        self.research_button = QPushButton("Générer un graphique", self)
+        self.map_button = QPushButton("Afficher une carte", self)
+
+        # Ajouter les boutons au layout horizontal
+        button_layout.addWidget(self.research_button)
+        button_layout.addWidget(self.map_button)
+
+        self.research_button.setFixedSize(150, 30)
+        self.map_button.setFixedSize(150, 30)
+
+        # Associer les fonctions aux boutons
+        self.research_button.clicked.connect(self.__show_graphic)
+        self.map_button.clicked.connect(self.__generatemap)
+
+
+        """ A faire, en cours de test
+        self.web_view = QWebEngineView(self)
+        central_layout.addWidget(self.web_view, alignment=Qt.AlignmentFlag.AlignCenter)
         """
 
-        # Supprimer la carte si elle existe (afin de la réactualiser par la suite)
+
+        # Créer le FigureCanvas pour afficher le graphique
+        self.figure = Figure()
+        self.figure.set_facecolor(self.palette().color(QPalette.ColorGroup.Current, QPalette.ColorRole.Dark).name()) # Mettre le fond du canvas a la même couleur que le fond du menu
+        self.canvas: FigureCanvas = FigureCanvas(self.figure)
+        central_layout.addWidget(self.canvas)
+        self.canvas.setMaximumSize(1000, 800) # Taille max du canvas
+        self.canvas.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding) # Fait en sorte que le canvas puisse s'étendre
+    
+    def init():
         if isfile("map.html"):
             remove("map.html")
 
-        self.window.iconbitmap(f"{self.api.basepath}\\interface\\icons\\icon-x32.ico")
-        self.window.title(self.title)
-        self.window.resizable(self.resizable, self.resizable)
-        self.window.minsize(self.minsize[0], self.minsize[1])
+        app = QApplication(sys.argv)
+        window = MainWindow()
+        window.show()
+        sys.exit(app.exec())
 
-        self.list_ville.configure(values=["==COMMUNES=="] + self.api.communes + ["==DEPARTEMENTS=="] + self.api.departements + ["==REGIONS=="] + self.api.regions)
-        self.list_ville.configure(justify='center')
-        self.list_ville.bind("<<ComboboxSelected>>", self.__on_selected)
-        self.list_ville.current(1)
+    def on_main_combo_box_changed(self):
+        if self.ignore_main_combo_box_change:
+            return
+        
+        selected_text = self.list_ville.currentText()
+        self.ignore_main_combo_box_change = True
 
-        self.list_ville.place(relx=0.5, y=30, anchor="center", x=30)
-        self.ville_label.place(relx=0.5, y=30, anchor="center", x=-60)
-        self.research_button.place(relx=0.5, y=80, anchor="center", x=-95)
-        self.map_button.place(relx=0.5, y=80, anchor="center", x=60)
-
-        self.canvas.bind('<Configure>', self.__on_resize)
-        self.canvas.pack(fill="both", expand=True)
-
-        self.window.iconbitmap(f"{self.api.basepath}\\interface\\icons\\icon-x32.ico")
-
-        self.window.mainloop()
-
-    def testinit(self) -> None:
-        """
-        Fonction testinit pour lancer une interface utilisée pour les tests
-        """
-
-        self.window.title(self.title)
-        self.window.resizable(self.resizable, self.resizable)
-        self.window.minsize(self.minsize[0], self.minsize[1])
-
-        self.list_ville.configure(values=["==COMMUNES=="] + self.api.communes + ["==DEPARTEMENTS=="] + self.api.departements + ["==REGIONS=="] + self.api.regions)
-        self.list_ville.bind("<<ComboboxSelected>>", self.__on_selected)
-        self.list_ville.current(1)
-
-        self.list_ville.place(relx=0.5, y=30, anchor="center", x=35)
-        self.ville_label.place(relx=0.5, y=30, anchor="center", x=-55)
-        self.research_button.place(relx=0.5, y=80, anchor="center", x=-60)
-        self.map_button.place(relx=0.5, y=80, anchor="center", x=40)
-
-        self.__show_graphic()
-        self.__generatemap(False)
-
-        self.close()
-
-    def close(self) -> None:
-        """
-        Fonction close pour fermer l'interface
-        """
-
-        self.window.destroy()
-
-    # Fonction privé qui permet d'afficher le graphique sur l'interface
-    def __show_graphic(self) -> None:
-        try:
-            inputdata = self.list_ville.get()
-            data = self.api.getCO2(self.dataname, inputdata)
-            dates = list(data.keys())
-            totalco2 = list(data.values())
-
-            self.canvas.delete("all") # supprimer l'image de fond
-
-            fig, ax = plt.subplots(num="GES")
-            ax.set_title(f"Bilan GES {self.dataname[:-1]} {inputdata}")
-            ax.bar(dates, totalco2, label="CO2")
-
-            plt.xlabel('Dates')
-            plt.ylabel('Tonnes de CO2')
-
-            plt.xticks(rotation=90) # dates à la verticale
-            ax.legend()
-
-            fig.set_figwidth(fig.get_figwidth() * 1.2)
-            fig.set_figheight(fig.get_figheight() * 1.2)
-
-            graphic = Figure(fig, master=self.window)
-
-            if self.graphic_widget is not None:
-                self.graphic_widget.destroy()
-
-            self.graphic_widget = graphic.get_tk_widget()
-            self.graphic_widget.place(relx=0.5, y=500, anchor="center")
-
-            self.window.minsize(self.minsize[0] + 600, self.minsize[1] + 680)
-
-            plt.close()
-        except HTTPError as e:
-            if self.tests: raise e
-            messagebox.showerror("Erreur", "Erreur de requete vers l'API: " + str(e.response))
-
-    # Fonction privé pour générer une carte en fonction du CO2
-    def __generatemap(self, save: bool = True) -> None:
-        if save:
-            self.map.save("map.html")
-
-    # Fonction privé (evenement) qui ajuste la taille de la barre de selection
-    def __on_selected(self, event) -> None:
-        if self.list_ville.get().startswith("=="):
-            self.list_ville.current(1)
+        if selected_text == "Commune":
             self.dataname = "Communes"
-            self.ville_label.configure(text="Commune : ")
-            self.list_ville.place_configure(x=(len(self.list_ville.get()) * 3) + 12)
-            self.list_ville.configure(width=len(self.list_ville.get()) + 5)
-        else:
-            self.list_ville.configure(width=len(self.list_ville.get()) + 5)
+            self.list_ville.clear()
+            self.list_ville.addItems(["Retour"] + self.api.communes)
+            self.list_ville.setCurrentIndex(1)
 
-            current = self.list_ville.current()
-            values = self.list_ville.configure().items().mapping["values"][4]
-            if current > values.index("==COMMUNES==") and current < values.index("==DEPARTEMENTS=="):
-                self.dataname = "Communes"
-                self.ville_label.configure(text="Commune : ")
-                self.list_ville.place_configure(x=len(self.list_ville.get()) * 3 + 12)
-            elif current > values.index("==DEPARTEMENTS==") and current < values.index("==REGIONS=="):
-                self.dataname = "Départements"
-                self.ville_label.configure(text="Département : ")
-                self.list_ville.place_configure(x=(len(self.list_ville.get()) * 3) + 14)
-            else:
-                self.dataname = "Régions"
-                self.ville_label.configure(text="Région : ")
-                self.list_ville.place_configure(x=(len(self.list_ville.get()) * 3) + 2)
-    
-    # Fonction privé (evenement) qui ajuste la taille du fond lorsque l'on redimentionne la taille de la fenetre
-    def __on_resize(self, event) -> None:
-        if not self.graphic_widget:
-            x = event.width
-            y = event.height
-            background_image = self.img.resize((x,y),Image.Resampling.BILINEAR)
-            self.background_photo = ImageTk.PhotoImage(background_image)
-            long, larg = background_image.size
+        elif selected_text == "Région":
+            self.dataname = "Régions"
+            self.list_ville.clear()
+            self.list_ville.addItems(["Retour"] + self.api.regions)
+            self.list_ville.setCurrentIndex(1)
 
-            x = (x - long) // 2
-            y = (y - larg) // 2
+        elif selected_text == "Département":
+            self.dataname = "Départements"
+            self.list_ville.clear()
+            self.list_ville.addItems(["Retour"] + self.api.departements)
+            self.list_ville.setCurrentIndex(1)
+            
+        elif selected_text == "Retour":
+            self.list_ville.clear()
+            self.list_ville.addItems(["Choisissez le type de localité","Commune", "Région", "Département"])
+            self.list_ville.setCurrentIndex(0)
+        
+        self.list_ville.setCurrentText(selected_text)
+        self.ignore_main_combo_box_change = False
 
-            self.canvas.delete("all")
-            self.canvas.create_image(x,y , image=self.background_photo, anchor="nw")
-            self.canvas.create_image(x,y , image=self.background_photo, anchor="nw")
+
+    def __generatemap(self):
+        self.map.save("map.html")
+
+
+    def __show_graphic(self):
+        try:
+            if not self.list_ville.currentText() in ["Retour","Commune", "Région", "Département","Choisissez le type de localité"]:
+                inputdata = self.list_ville.currentText()
+                data = self.api.getCO2(self.dataname, inputdata)
+                dates = list(data.keys())
+                totalco2 = list(data.values())
+
+                # Effacer le contenu précédent du graphique
+                self.figure.clear()
+
+                # Récupérer l'axe de la figure
+                ax = self.figure.subplots()
+
+                # Créer le graphique
+                ax.bar(dates, totalco2, label="CO2")
+                ax.set_title(f"Bilan GES, {self.dataname[0:-1]} : {inputdata}", color="white")
+                ax.set_xlabel('Dates',  color="white")
+                ax.set_ylabel('Tonnes de CO2',  color="white")
+                ax.legend()
+                ax.tick_params(axis='x', rotation=90)
+                ax.tick_params(labelcolor='white')
+
+                # Mettre à jour le graphique dans le canvas
+                self.figure.subplots_adjust(bottom=0.25)
+                self.canvas.draw()
+        except HTTPError as e:
+            error_message = f"Erreur de requête vers l'API: {str(e.response)}"
+            QMessageBox.critical(None, "Erreur", error_message)
+
+
+MainWindow.init()
