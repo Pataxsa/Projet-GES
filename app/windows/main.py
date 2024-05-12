523cb163243001129b8f5fb68934090dfd3ee9f9
Noam <noamchamouard@outlook.fr>
2024-05-12 08:57:09 +0200

    ajout d'un stacked widget pour charger les pages plus vite

--- app/windows/main.py
+++ app/windows/main.py
@@ -1,10 +1,4 @@
-"""
-Interface principale
-"""
-
-# TODO: changer la barre de menu + ajouter stacked widget (pour les pages) + rajouter de la docstring + autres trucs...
-
-from PySide6.QtWidgets import QMainWindow, QWidget, QPushButton, QVBoxLayout, QComboBox, QSizePolicy, QMenu, QHBoxLayout
+from PySide6.QtWidgets import QMainWindow, QWidget, QPushButton, QVBoxLayout, QComboBox, QSizePolicy, QMenu, QHBoxLayout, QStackedWidget
 from PySide6.QtCore import Qt, QUrl
 from PySide6.QtGui import QPalette, QIcon
 from PySide6.QtWebEngineCore import QWebEngineSettings
@@ -41,14 +35,6 @@ class Main(QMainWindow):
         self.central_layout.setContentsMargins(20, 20, 20, 20)
         self.central_layout.setAlignment(Qt.AlignmentFlag.AlignCenter)  # Centrer les éléments verticalement
 
-        # Créer une combobox principale
-        self.list_ville = QComboBox(self)
-        self.list_ville.addItems(["Choisissez le type de localité","Commune", "Région", "Département"])
-        self.list_ville.setStyleSheet("QComboBox { min-width: 200px; }")  # Définir une largeur minimale
-        self.list_ville.currentIndexChanged.connect(self.on_main_combo_box_changed)
-
-        self.central_layout.addWidget(self.list_ville, alignment=Qt.AlignmentFlag.AlignHCenter)  # Centrer la combobox horizontalement
-
         # Créer un menu pour afficher les options
         self.menu = QMenu(self)
 
@@ -63,59 +49,56 @@ class Main(QMainWindow):
         self.menu_button.setIcon(QIcon(f"{RESOURCE_PATH}\\icons\\icon-x32.ico").pixmap(30,30))
 
         # Ajouter le bouton en haut à gauche
-        self.menu_button.setGeometry(0, 0, 100, 30)
+        self.central_layout.addWidget(self.menu_button)
 
-        # Créer un layout horizontal pour les boutons du menu et de génération de graphiques
-        button_layout = QHBoxLayout()
+        # Créer une combobox principale
+        self.list_ville = QComboBox(self)
+        self.list_ville.addItems(["Choisissez le type de localité","Commune", "Région", "Département"])
+        self.list_ville.setStyleSheet("QComboBox { min-width: 200px; }")  # Définir une largeur minimale
+        self.list_ville.currentIndexChanged.connect(self.on_main_combo_box_changed)
 
-        # Ajouter un espace extensible à gauche
-        button_layout.addStretch(1)
+        self.central_layout.addWidget(self.list_ville, alignment=Qt.AlignmentFlag.AlignHCenter)  # Centrer la combobox horizontalement
 
-        # Créer un bouton pour générer des graphiques
-        self.graphic_button = QPushButton("Générer le graphique", self)
-        self.graphic_button.setFixedSize(150, 30)
-        self.graphic_button.clicked.connect(self.__show_graphic)
+        # Créer un stacked widget pour gérer les pages
+        self.stacked_widget = QStackedWidget()
+        self.central_layout.addWidget(self.stacked_widget)
 
-        # Ajouter le bouton de génération de graphiques au layout horizontal
-        button_layout.addWidget(self.graphic_button)
+        # Créer les pages et les ajouter au stacked widget
+        self.map_page = QWidget()
+        self.stacked_widget.addWidget(self.map_page)
 
-        # Créer un bouton pour enregistrer les graphiques
-        self.save_button = QPushButton("Enregistrer le graphique", self)
-        self.save_button.setFixedSize(150, 30)
-        self.save_button.clicked.connect(self.save_graph)
+        self.graph_page = QWidget()
+        self.stacked_widget.addWidget(self.graph_page)
 
-        # Ajouter le bouton pour enregistrer les graphiques au layout horizontal
-        button_layout.addWidget(self.save_button)
+        # Afficher initialement la page de la carte
+        self.stacked_widget.setCurrentWidget(self.map_page)
 
-        # Aligner les éléments à droite
-        button_layout.addStretch(1)
+        # Créer les éléments pour la page de la carte
+        self.setup_map_page()
 
-        # Ajouter le layout horizontal au layout vertical central
-        self.central_layout.addLayout(button_layout)
+        # Créer les éléments pour la page du graphique
+        self.setup_graph_page()
 
-        # Masquer le bouton jusqu'à ce qu'un graphique soit généré
-        self.save_button.hide()
+    def setup_map_page(self):
+        """Configurer les éléments de la page de la carte"""
+        layout = QVBoxLayout(self.map_page)
+        self.map_view = QWebEngineView()
+        file_path = abspath("map.html")
+        self.map_view.settings().setAttribute(QWebEngineSettings.WebAttribute.LocalContentCanAccessRemoteUrls, True)
+        self.map_view.setUrl(QUrl.fromLocalFile(file_path))
+        layout.addWidget(self.map_view)
+
+    def setup_graph_page(self):
+        """Configurer les éléments de la page du graphique"""
+        layout = QVBoxLayout(self.graph_page)
 
         # Créer le FigureCanvas pour afficher le graphique
         self.figure = Figure()
-        self.figure.set_facecolor(central_widget.palette().color(QPalette.ColorRole.Window).name()) # Mettre le fond du canvas a la même couleur que le fond du menu
+        self.figure.set_facecolor(self.graph_page.palette().color(QPalette.ColorRole.Window).name()) # Mettre le fond du canvas a la même couleur que le fond du menu
         self.canvas: FigureCanvas = FigureCanvas(self.figure)
-        self.central_layout.addWidget(self.canvas)
+        layout.addWidget(self.canvas)
         self.canvas.setMaximumSize(1000, 800) # Taille max du canvas
         self.canvas.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding) # Fait en sorte que le canvas puisse s'étendre
-
-
-        # TODO: très lent (attribute et setUrl, ne pas charger cela au lancement de l'app mais plutot lorsque cela est nécessaire)
-        self.web_view = QWebEngineView()
-        file_path = abspath("map.html")
-        self.web_view.settings().setAttribute(QWebEngineSettings.WebAttribute.LocalContentCanAccessRemoteUrls, True)
-        self.web_view.setUrl(QUrl.fromLocalFile(file_path))
-
-        # Ajouter la carte, masquée
-        self.central_layout.addWidget(self.web_view)
-        self.web_view.hide()
-        self.list_ville.hide()
-        self.graphic_button.hide()
 
     def on_main_combo_box_changed(self):
         
@@ -140,40 +123,9 @@ class Main(QMainWindow):
             self.figure.savefig(f"bilan_GES_{self.dataname[0:-1]}_{self.list_ville.currentText()}")
 
     def __showMap(self):
-        #cacher widgets pour faire des graphiques
-        self.canvas.hide()
-        self.list_ville.hide()
-        self.graphic_button.hide()
-        self.save_button.hide()
-        self.web_view.show()
+        """Afficher la page de la carte"""
+        self.stacked_widget.setCurrentWidget(self.map_page)
 
     def __show_graphic(self):
-        self.web_view.hide()
-        self.list_ville.show()
-        self.graphic_button.show()
-        self.canvas.show()
-        if not self.list_ville.currentText() in ["Retour","Commune", "Région", "Département","Choisissez le type de localité"]:
-            inputdata = self.list_ville.currentText()
-            data = self.api.getCO2(self.dataname, inputdata)
-            dates = list(data.keys())
-            totalco2 = list(data.values())
-
-            # Effacer le contenu précédent du graphique
-            self.figure.clear()
-
-            # Récupérer l'axe de la figure
-            ax = self.figure.subplots()
-
-            # Créer le graphique
-            ax.bar(dates, totalco2, label="CO2")
-            ax.set_title(f"Bilan GES, {self.dataname[0:-1]} : {inputdata}", color="white")
-            ax.set_xlabel('Dates',  color="white")
-            ax.set_ylabel('Tonnes de CO2',  color="white")
-            ax.legend()
-            ax.tick_params(axis='x', rotation=90)
-            ax.tick_params(labelcolor='white')
-
-            # Mettre à jour le graphique dans le canvas
-            self.figure.subplots_adjust(bottom=0.25)
-            self.canvas.draw()
-            self.save_button.show()
+        """Afficher la page du graphique"""
+        self.stacked_widget.setCurrentWidget(self.graph_page)
